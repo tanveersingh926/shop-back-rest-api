fba1d0ab58f1d4378f9209aa2bc3435b9bfe5e4d
tanveersingh926 <tanveer.singh926@gmail.com>
2018-05-08 19:59:35 +0800

    Error handling done and logger added

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productsRoutes = require('./api/routes/products')
 const ordersRoutes = require('./api/routes/orders')
 
+app.use(morgan('dev'));
+
+// Routes which should handle requests
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
+
+app.use((req,res,next)=>{
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            code:error.status,
+            message:error.message
+        }
+    });
+});
 
 module.exports = app;
