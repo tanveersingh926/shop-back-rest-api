2d79cd44f7043ffdb43776863ccdc623c714d4da
tanveersingh926 <tanveer.singh926@gmail.com>
2018-05-17 15:58:10 +0800

    Modified Schema and customized response for Orders API

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,37 +3,123 @@ const router = express.Router();
 
 const mongoose = require('mongoose');
 const Order = require('../models/order')
+const Product = require('../models/product')
 
 router.get('/', function(req,res){
-    res.status(200).json({
-        message:'order was fetched'
+    Order.find()
+    .select('_id product quantity')
+    .exec()
+    .then(docs => {
+        res.status(200).json({
+            count:docs.length,
+            orders:docs.map(doc => {
+                return {
+                    _id:doc._id,
+                    quantity:doc.quantity,
+                    product:doc.product,
+                    request:{
+                        type:'GET',
+                        url:'http://localhost:3001/orders/'+doc._id
+                    }
+        
+                }
+            }),
+        });
+    })
+    .catch(err=>{
+        res.status(500).json(err)
     })
 });
 
 
 router.post('/', function(req,res){
-    const order = {
-        productId:req.body.productId
-        , quantity: req.body.quantity
-    }
-    res.status(201).json({
-        message:'order was created',
-        order:order
+
+    Product.findById(req.body.productId)
+    .then(product => {
+
+        if(!product){
+            console.log("product not found ", product)
+            return res.status(404).json({
+                message:"Product not found."
+            })
+        }
+
+        const order = new Order({
+            _id:new mongoose.Types.ObjectId()
+            , product: req.body.productId
+            , quantity: req.body.quantity
+        })
+
+        return order.save()
+    })
+
+    .then(result => {
+        console.log(result)
+        res.status(201).json({
+            message:'Order Stored.',
+            createdOrder:{
+                _id:result._id,
+                quantity:result.quantity,
+                product:result.product
+            },
+            request:{
+                type:'POST',
+                url:'http://localhost:3001/orders/'+result._id
+            }
+
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
     })
 });
 
 router.get('/:orderId', function(req,res){
-    res.status(200).json({
-        message:'order details',
-        orderId:req.params.orderId
+    const id = req.params.orderId;
+
+    Order.find({_id:id})
+    .exec()
+    .then(doc => {
+        if(!doc){
+            return res.status(200).json({message:"Order not found."})
+        } 
+
+        res.status(200).json({
+            orders:doc,
+            request:{
+                type:'GET',
+                url:'http://localhost:3001/orders/'+id
+            }
+        });
     })
+    .catch(err=>{
+        res.status(500).json(err)
+    })
+
+   
 });
 
 router.delete('/:orderId', function(req,res){
-    res.status(201).json({
-        message:'order was deleted',
-        orderId:req.params.orderId
+    const id = req.params.orderId;
+
+    Order.remove({_id:id})
+    .exec()
+    .then(result =>{
+        res.status(201).json({
+            message:'Order was deleted',
+            request:{
+                type:"GET",
+                url:'http://localhost:3001/orders/',
+                desription:'Get all orders.'
+            }
+        })
+    
     })
+    .catch(err=>{
+        res.status(500).json(err)
+    })
+
 });
 
 module.exports = router;
