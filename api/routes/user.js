d9d0fa8874e8458538cb53342e282e9cca632dd2
tanveersingh926 <tanveer.singh926@gmail.com>
2018-05-18 17:31:12 +0800

    added JWT and generated token on base of that

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/user')
 
@@ -47,6 +48,55 @@ router.post('/signup', (req, res, next)=>{
 
 })
 
+router.post('/login',(req,res,next)=>{
+    User.find({email:req.body.email})
+    .exec()
+    .then(user => {
+        if(user.length < 1) {
+            res.status(401).json({
+                message:"Auth failed"
+            })
+        } else {
+            bcrypt.compare(req.body.password, user[0].password, (err, result)=>{
+                if(err){
+                    return res.status(401).json({
+                        message:"Auth failed"
+                    })
+                } 
+                
+                if(result){
+                    const token = jwt.sign(
+                        {
+                            email:user[0].email,
+                            _id:user[0]._id,
+                        }, 
+                        process.env.PRIVATE_JWT_KEY,
+                        {
+                            expiresIn:"1h"
+                        }
+                    );
+
+
+                    return res.status(200).json({
+                        message:"Auth Successful",
+                        token:token
+                    })
+                }
+
+                res.status(401).json({
+                    message:"Auth failed"
+                })
+    
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+})
 
 router.delete("/:userId", (req, res, next) => {
     User.remove({ _id: req.params.userId })
